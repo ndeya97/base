b6954bfbd0111fd948d07b41b42c22ea1038e1d9
ndeya97 <nadiop97@gmail.com>
2024-09-24 17:40:22 +0200

    TP de base résolu

--- src/presentation/Main.java
+++ src/presentation/Main.java
@@ -1,8 +1,13 @@
 package presentation;
 
+import sn.isi.controller.Fonction;
+
 public class Main {
 	public static void main(String[] args) {
+		//Creation d'une instance de la classe Fonction
+		Fonction fonction = new Fonction();
 		
+		//Appel du sous-programme pour saisir et afficher le montant TTC
+		fonction.saisirProduitCalculerEtAfficherMontantTTC();			
 	}
-
 }
--- src/sn/isi/controller/Fonction.java
+++ src/sn/isi/controller/Fonction.java
@@ -3,21 +3,37 @@ package sn.isi.controller;
 import java.util.Scanner;
 
 public class Fonction {
+	//Constante TVA 0
+	private static final double TVA = 0.18;
+
 	//Sous-programme qui saisit les informations d'un produit puis calcul et affiche le montant TTC
-	private double SaisirProduitCalculerEtAfficherMontantTTC() {
+	public void saisirProduitCalculerEtAfficherMontantTTC() {
 		//Saisie des informations du produit
 		Scanner scanner = new Scanner(System.in);
 		
+		//Saisie du nom du produit
 		System.out.print("Entrez le nom du produit : ");
 		String nom = scanner.nextLine();
 		
+		//Saisie de la quantite du produit
 		System.out.print("Entrez la quantité du produit : ");
-		String quantite = scanner.nextInt();
+		int quantite = Integer.parseInt(scanner.nextLine());
 		
-		System.out.print("Entrez le prix HT du produit : ");
-		String prix = scanner.nextDouble();
-		
+		//Saisie du prix HT du produit
+		System.out.print("Entrez le prix HT du produit (€) : ");
+		double prixHT;
+	
+		try {
+		 prixHT = Double.parseDouble(scanner.nextLine());
+		} catch (NumberFormatException e) {
+			System.out.println ("Erreur : le prix doit être un nombre valide");
+			return; //Sort de la méthode sans rien faire
+		}
+			
 		//Calcul du montant TTC
+		double montantTTC = quantite * prixHT * (1 + TVA); 
+		
 		//Affichage du montant TTC
+		System.out.printf("Le montant TTC pour %d %s  est : %.2f €\n", quantite, nom, montantTTC);
 	}
 }
